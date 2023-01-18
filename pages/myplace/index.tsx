d90eb0a43d1571cb7745a048c384b863816b88cf
Joonil Kim <hydralisk.ks@gmail.com>
2023-01-18 12:57:27 -0700

    minor bug fixed

--- pages/myplace/index.tsx
+++ pages/myplace/index.tsx
@@ -3,14 +3,11 @@ import Skills from "./skills"
 import Detail from "./detail"
 import Projects from "./projects"
 import Contacts from "./contacts"
-import Head from "next/head"
 import Script from "next/script"
 
 export default function MyPlace() {
   return (<>
-    <Head>
-      <Script src="https://kit.fontawesome.com/40069747e9.js" crossOrigin="anonymous"></Script>
-    </Head>
+    <Script src="https://kit.fontawesome.com/40069747e9.js" crossOrigin="anonymous"></Script>
     <About />
     <Detail />
     <Skills />
