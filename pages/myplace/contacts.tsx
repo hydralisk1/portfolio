f3fbd14e6d02db945947c8010771475c3c12cd48
Joonil Kim <hydralisk.ks@gmail.com>
2023-01-18 12:48:47 -0700

    contacts

--- pages/myplace/contacts.tsx
+++ pages/myplace/contacts.tsx
@@ -1,3 +1,28 @@
+import styles from './contacts.module.css'
+
 export default function Contacts() {
-  return <div>Hello World</div>
+  const style = {
+    color: 'white',
+    fontSize: '80px',
+    marginRight: '2rem'
+  }
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.contactsContainer}>
+        <div className={styles.contactsTag}>You can contact me at</div>
+        <div className={styles.contacts}>
+          <a href='https://www.linkedin.com/in/joonil-kim-hello/' target='_blank' rel="noreferrer">
+            <i style={style} className="fa-brands fa-linkedin"></i>
+          </a>
+          <a href='https://github.com/hydralisk1' target='_blank' rel="noreferrer">
+            <i style={style} className="fa-brands fa-square-github"></i>
+          </a>
+          <a href='mailto:bartholomaeuskim@gmail.com'>
+            <i style={style} className="fa-solid fa-envelope"></i>
+          </a>
+        </div>
+      </div>
+    </div>
+  )
 }
--- pages/myplace/index.tsx
+++ pages/myplace/index.tsx
@@ -2,14 +2,18 @@ import About from "./about"
 import Skills from "./skills"
 import Detail from "./detail"
 import Projects from "./projects"
-// import Contacts from "./contacts"
+import Contacts from "./contacts"
+import Head from "next/head"
 
 export default function MyPlace() {
   return (<>
+    <Head>
+      <script src="https://kit.fontawesome.com/40069747e9.js" crossOrigin="anonymous"></script>
+    </Head>
     <About />
     <Detail />
     <Skills />
     <Projects />
-    {/* <Contacts /> */}
+    <Contacts />
   </>)
 }
