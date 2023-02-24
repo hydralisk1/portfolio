b96cc1419079a8fc88b763786e4aa7ce5ca405ec
Joonil Kim <hydralisk.ks@gmail.com>
2023-02-24 16:31:09 -0700

    project links modified

--- pages/myplace/projects.tsx
+++ pages/myplace/projects.tsx
@@ -9,7 +9,7 @@ export default function Projects() {
     name: 'Mannam - Inspired by Meetup',
     detail: 'Mannam is a Meetup cloning site. Mannam can be used for creating groups, creating events and attending other\'s events.\n\nThis application is powered by Google Maps API, Google Geocoding API. \n\nAs a side note, Mannam literally means MeetUp in Korean.',
     image: mannamPic,
-    url: 'https://mannam.onrender.com',
+    url: 'https://mannamco.com',
   },{
     name: 'Rockethood | Comission-free Stock Trading & Investing App',
     detail: 'Rockethood is a website clone inspired by Robinhood. This project was created by Efrain, Ha, Joonil, and Kat from App Academy as a part of our coursework. Our project is a fully-functional web application that allows users to buy and sell stocks, track stocks performance,and bookmark the finance news using real-time market data. We built the application using Python and JavaScript, with the Flask framework for the backend and React/Redux for the frontend.',
@@ -19,7 +19,7 @@ export default function Projects() {
     name: 'Itzy - Full-stack e-commerce web application',
     detail: 'Itzy is a website clone inspired by Etsy. This project was created by Joonil from App Academy. This project is a fully-functional web application that allows users to buy and sell products. I built the application using Python and JavaScript, with the Flask framework for the backend and React/Redux for the frontend. I also used SQLAlchemy to handle our database interactions and Boto3 for AWS integration.',
     image: itzyPic,
-    url: 'https://itzy.onrender.com',
+    url: 'https://itzyco.com',
   }]
 
   return (<>
